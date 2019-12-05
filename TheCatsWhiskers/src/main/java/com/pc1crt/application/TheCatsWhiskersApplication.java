5a4f33ef5ea5b4af6c45074b0c99847c0dc0f0d3
paul_ <paul_@DESKTOP-T5G9QRL>
2019-12-05 14:02:03 +0000

    added react properties

--- TheCatsWhiskers/src/main/java/com/pc1crt/application/TheCatsWhiskersApplication.java
+++ TheCatsWhiskers/src/main/java/com/pc1crt/application/TheCatsWhiskersApplication.java
@@ -2,10 +2,8 @@ package com.pc1crt.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.oauth2.client.test.OAuth2ContextConfiguration;
 
 @SpringBootApplication
-@OAuth2ContextConfiguration
 public class TheCatsWhiskersApplication {
 
 	public static void main(String[] args) {
--- TheCatsWhiskers/src/main/java/com/pc1crt/application/controller/WelcomeController.java
+++ TheCatsWhiskers/src/main/java/com/pc1crt/application/controller/WelcomeController.java
@@ -1,5 +1,8 @@
 package com.pc1crt.application.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
@@ -8,42 +11,49 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.pc1crt.application.model.Cat;
 import com.pc1crt.application.repositories.CatRepository;
 
 @Controller
 public class WelcomeController {
 
-    // inject via application.properties
-    @Value("${welcome.message}")
-    private String message;
+	// inject via application.properties
+	@Value("${welcome.message}")
+	private String message;
 
-    @Autowired
-    CatRepository catRepository;
-    @GetMapping("/")
-    public String main(Model model) {
-        model.addAttribute("message", message);
-        model.addAttribute("tasks", catRepository.findAll());
+	@Autowired
+	CatRepository catRepository;
 
-        return "home"; //view
-    }
+	public List<String> catName= new ArrayList<String>();
 
-  
+	@GetMapping("/")
+	public String main(Model model) {
+		catName.clear();
+		List<Cat>cats = catRepository.findAll();
+		for(Cat cat:cats) {
+			catName.add(cat.getName());
+		}
+		model.addAttribute("message", message);
 
-    // /hello?name=kotlin
-    @GetMapping("/hello")
-    public String mainWithParam(
-            @RequestParam(name = "name", required = false, defaultValue = "User") 
-			String name, Model model) {
+		model.addAttribute("tasks", catName);
 
-        model.addAttribute("message", name);
+		return "home"; // view
+	}
 
-        return "alt"; //view
-    }
-    @RequestMapping("/start")
-    public String Start() {
-    	
-    	
-    	return "start";
-    }
+	// /hello?name=kotlin
+	@GetMapping("/hello")
+	public String mainWithParam(@RequestParam(name = "name", required = false, defaultValue = "User") String name,
+			Model model) {
+
+		model.addAttribute("message", name);
+
+		return "alt"; // view
+	}
+
+	@RequestMapping("/start")
+	public String Start() {
+
+		return "start";
+	}
 
 }
