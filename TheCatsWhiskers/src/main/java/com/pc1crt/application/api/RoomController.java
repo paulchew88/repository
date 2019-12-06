0f3686e09f0793a5549f7e1fef7569fe3e6c1f40
paulchew88 <paul_chew@yahoo.co.uk>
2019-12-06 10:36:57 +0000

    l

---
+++ TheCatsWhiskers/src/main/java/com/pc1crt/application/api/RoomController.java
@@ -0,0 +1,5 @@
+package com.pc1crt.application.api;
+
+public class RoomController {
+
+}
--- TheCatsWhiskers/src/main/java/com/pc1crt/application/model/AuthorityType.java
+++
@@ -1,6 +0,0 @@
-package com.pc1crt.application.model;
-
-public enum AuthorityType {
-	  ROLE_ADMIN,
-	    ROLE_USER;
-}
